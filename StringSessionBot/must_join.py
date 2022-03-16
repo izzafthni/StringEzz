ed37d7b0ad4757471ee5c6e540d660c57634300f
izzafthni <99348047+izzafthni@users.noreply.github.com>
2022-03-16 23:05:14 +0700

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,7 +19,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"join channel terlebih dahulu[this channel]({link}) untuk mengambil string. Setelah join channel coba start kembali!",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
